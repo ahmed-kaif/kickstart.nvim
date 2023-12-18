2c528ff67db7c709960cd4cf4699d13225446262
ahmed-kaif <kaifkhan.rcs@gmail.com>
2023-12-18 20:13:28 +0600

    telescope dropdown blend fixed

--- init.lua
+++ init.lua
@@ -244,7 +244,7 @@ require('lazy').setup({
         },
         transparent_background = true, -- disables setting the background color.
         show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
         dim_inactive = {
           enabled = false, -- dims the background color of inactive window
           shade = "dark",
@@ -283,7 +283,6 @@ require('lazy').setup({
           },
           telescope = {
             enabled = true,
-            style = "classic"
           },
         },
       })
@@ -502,7 +501,7 @@ vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { d
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
+    winblend = 0,
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
