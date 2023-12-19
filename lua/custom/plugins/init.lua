2722a3f20b990a860123b1de891b7df108c5b366
ahmed-kaif <kaifkhan.rcs@gmail.com>
2023-12-20 00:01:20 +0600

    added bufferline plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -56,4 +56,17 @@ return {
       })
     end,
   },
+  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons',
+    after="catppuccin",
+    config = function ()
+      require("bufferline").setup{
+        options = {
+          numbers = function (options)
+            return string.format('%s', options.raise(options.ordinal))
+          end,
+        },
+        highlights = require("catppuccin.groups.integrations.bufferline").get()
+      }
+    end
+  }
 }
